a5b241b4357b29da26335a460b74e3e2bef19952
ali993abbas <ali993abbas@gmail.com>
2020-03-16 12:56:45 +0500

    first commit

---
+++ btn.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Button = ({ styleClass, value, onClick }) => (
+  <button className={`btn ${styleClass}`} onClick={event => onClick(event)}>
+    {value}
+  </button>
+);
+
+Button.propTypes = {
+  styleClass: PropTypes.string,
+  value: PropTypes.string.isRequired,
+  onClick: PropTypes.string
+};
+
+Button.defaultProps = {
+  styleClass: "btn-primary"
+};
+
+export default Button;
